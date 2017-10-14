847b910880c44840f2eecf128e24746010453221
IED Student <iedstudent@ied.edu>
2017-10-14 12:36:56 +0200

    Choice.cs

---
+++ Assets/Scripts/Choice.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Choice : MonoBehaviour {
+
+    string phrase;
+    int value;
+
+    public Choice(string phrase, int value)
+    {
+        this.phrase = phrase;
+        this.value = value;
+    }
+}
--- Assets/Scripts/Controller.cs
+++ Assets/Scripts/Controller.cs
@@ -6,7 +6,11 @@ public class Controller : MonoBehaviour {
 
     public Character character;
 	
-	void Update () {
-		
+	void Update() {
+        if(Input.GetButtonDown("Fire1")) {
+            character.ChangeExpression(0);
+        } else if(Input.GetButtonDown("Fire2")) {
+            character.ChangeExpression(1);
+        }
 	}
 }
