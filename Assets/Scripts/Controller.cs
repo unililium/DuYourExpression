fa6fba40e8bdf98c4d1127c095dd295826cd3280
Du Lin <lnidux@gmail.com>
2017-10-14 14:23:44 +0200

    buttons

--- Assets/Scripts/Controller.cs
+++ Assets/Scripts/Controller.cs
@@ -1,16 +1,33 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Controller : MonoBehaviour {
 
     public Character character;
-	
-	void Update() {
+
+    public Button button_a;
+    public Button button_b;
+
+    void Start() {
+    }
+
+    void Update() {
         if(Input.GetButtonDown("Fire1")) {
             character.ChangeExpression(0);
         } else if(Input.GetButtonDown("Fire2")) {
             character.ChangeExpression(1);
         }
 	}
+
+    public void OnClickChange(Button button) {
+        Choice choice = button.GetComponentInChildren<Choice>();
+        character.ChangeExpression(choice.value);
+        Debug.Log(choice.phrase);
+        Text text = button.GetComponentInChildren<Text>();
+        text.text = choice.phrase;
+        
+        character.NextChoices();
+    }
 }
