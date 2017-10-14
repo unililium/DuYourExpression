54230c1ada5665ef6d887baafc84b1a235e6d884
IED Student <iedstudent@ied.edu>
2017-10-14 11:38:42 +0200

    Character enums controller

---
+++ Assets/Scripts/Enums.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enums : MonoBehaviour {
+
+    public enum Expression { Smiling, Sad, Neutral, Angry };
+
+}
